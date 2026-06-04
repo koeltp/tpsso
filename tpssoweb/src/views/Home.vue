5e5e6de5ebb4d9cea20dbdfcc11f68f506b7c32e
tp <tp@taipi.top>
2026-06-05 01:26:33 +0800

    refactor: 替换内联SVG图标为本地图片资源，优化页面配置
    1.  新增favicon.png、logo.png、logo-icon.png等静态资源，删除无用的vue.svg和vite.svg
    2.  替换导航栏和登录页的内联SVG为图片标签，更新网站标题和页面文案
    3.  调整登录页logo区域的样式适配图片显示

--- tpssoweb/src/views/Home.vue
+++ tpssoweb/src/views/Home.vue
@@ -3,10 +3,8 @@
     <nav class="navbar">
       <div class="navbar-inner">
         <router-link to="/" class="navbar-brand">
-          <svg width="28" height="28" viewBox="0 0 48 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M25.946 44.938c-.664.845-2.021.375-2.021-.698V33.937a2.26 2.26 0 0 0-2.262-2.262H10.287c-.92 0-1.456-1.04-.92-1.788l7.48-10.471c1.07-1.497 0-3.578-1.842-3.578H1.237c-.92 0-1.456-1.04-.92-1.788L10.013.474c.214-.297.556-.474.92-.474h28.894c.92 0 1.456 1.04.92 1.788l-7.48 10.471c-1.07 1.498 0 3.579 1.842 3.579h11.377c.943 0 1.473 1.088.89 1.83L25.947 44.94z" fill="#7c3aed"/>
-          </svg>
-          <span class="navbar-title">TPSSO</span>
+          <img :src="logoSrc" alt="TPSSO" width="28" height="28" class="navbar-logo" />
+          <span class="navbar-title">SSO</span>
         </router-link>
         <div class="navbar-right">
           <router-link to="/" class="nav-link">首页</router-link>
@@ -137,6 +135,7 @@ import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { Lock, Connection, Monitor, User, Setting, CircleCheck, Odometer, ArrowDown, SwitchButton } from '@element-plus/icons-vue'
 import api from '@/utils/api'
+import logoSrc from '@/assets/logo-icon.png'
 
 const router = useRouter()
 
